16fef05cf26829a892f71087ce7d2aceb74e845d
hishamss <hishamss90@gmail.com>
2020-04-26 14:22:55 -0700

    implemented home page

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require("express");
 
 var app = express();
 
-PORT = process.env.PORT || 3000;
+PORT = process.env.PORT || 8080;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./app/routing/htmlRoutes.js")(app);
-// require("/routing/apiRoutes.js")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, () => console.log("listning on http://localhost:" + PORT));
