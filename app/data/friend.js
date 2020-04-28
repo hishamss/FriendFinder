cd9541506246f1bcb4ad4772a841757f90c72d2c
hishamss <hishamss90@gmail.com>
2020-04-27 18:29:57 -0700

    created arraydifference

--- app/data/friend.js
+++ app/data/friend.js
@@ -11,40 +11,40 @@ var friends = [
       "https://www.biography.com/.image/t_share/MTE5NTU2MzE2MTkzMjYxMDY3/russell-crowe-9262435-1-402.jpg",
     scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"],
   },
-  {
-    name: "Jason Statham",
-    photo: "https://parade.com/wp-content/uploads/2018/07/JohnStatham-FTR.jpg",
-    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"],
-  },
-  {
-    name: "Louis T. Delia",
-    photo:
-      "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"],
-  },
-  {
-    name: "John Doe",
-    photo: "https://www.kayakbikebrew.com/wp-content/uploads/2019/04/team.png",
-    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"],
-  },
-  {
-    name: "Lil Wayne",
-    photo:
-      "https://www.biography.com/.image/t_share/MTQ3NjM5MzQyOTU5OTYxNDc2/lil_wayne_photo_by_ray_tamarra_getty_images_entertainment_getty_56680625.jpg",
-    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"],
-  },
-  {
-    name: "Cristiano Ronaldo",
-    photo:
-      "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d2388f14c687b00085c0f91%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D1559%26cropY1%3D130%26cropY2%3D1690",
-    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
-  },
-  {
-    name: "Lionel Messi",
-    photo:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Leo_Messi_2016.PNG/170px-Leo_Messi_2016.PNG",
-    scores: ["1", "2", "2", "3", "4", "3", "3", "5", "5", "1"],
-  },
+  //   {
+  //     name: "Jason Statham",
+  //     photo: "https://parade.com/wp-content/uploads/2018/07/JohnStatham-FTR.jpg",
+  //     scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"],
+  //   },
+  //   {
+  //     name: "Louis T. Delia",
+  //     photo:
+  //       "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+  //     scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"],
+  //   },
+  //   {
+  //     name: "John Doe",
+  //     photo: "https://www.kayakbikebrew.com/wp-content/uploads/2019/04/team.png",
+  //     scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"],
+  //   },
+  //   {
+  //     name: "Lil Wayne",
+  //     photo:
+  //       "https://www.biography.com/.image/t_share/MTQ3NjM5MzQyOTU5OTYxNDc2/lil_wayne_photo_by_ray_tamarra_getty_images_entertainment_getty_56680625.jpg",
+  //     scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"],
+  //   },
+  //   {
+  //     name: "Cristiano Ronaldo",
+  //     photo:
+  //       "https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d2388f14c687b00085c0f91%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D0%26cropX2%3D1559%26cropY1%3D130%26cropY2%3D1690",
+  //     scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
+  //   },
+  //   {
+  //     name: "Lionel Messi",
+  //     photo:
+  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Leo_Messi_2016.PNG/170px-Leo_Messi_2016.PNG",
+  //     scores: ["1", "2", "2", "3", "4", "3", "3", "5", "5", "1"],
+  //   },
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,28 @@ module.exports = function (app) {
     res.json(friends);
   });
   app.post("/api/friends", function (req, res) {
+    // var BestMatch = friends[totalDifference(req.body).index];
     friends.push(req.body);
-    res.json(friends);
+    res.json(totalDifference(req.body));
   });
 };
+
+function totalDifference(NewFriend) {
+  var NewScore = NewFriend.scores;
+  var Defferences = [];
+  for (friend of friends) {
+    Defferences.push(subtarray(friend.scores, NewScore));
+  }
+  return {
+    index: Defferences.indexOf(Math.min(...Defferences)),
+    deff: Defferences,
+  };
+}
+
+function subtarray(arr1, arr2) {
+  var TotalDefference = 0;
+  for (i = 0; i < arr1.length; i++) {
+    TotalDefference += Math.abs(arr1[i] - arr2[i]);
+  }
+  return TotalDefference;
+}
